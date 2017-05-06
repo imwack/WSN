e850a3ac0f9412392a6503c8844e412287eee388
imwack <i.am.wack.1993@gmail.com>
2017-05-06 23:26:13 +0800

    Add sensor network node class

---
+++ node.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+from numpy import random
+
+
+'''
+无限传感网络节点类
+坐标(x,y)
+能量energy
+'''
+
+
+class Node(object):
+    x = 0
+    y = 0
+    energy = 0.0
+    '''构造函数'''
+    def __init__(self, width=1000, height=1000):
+        self.x = random.randint(1, width)   # [1,width]
+        self.y = random.randint(1, height)  # [1,height]
+        self.energy = random.rand()         # [0,1]浮点数
+
