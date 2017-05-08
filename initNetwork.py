7dc08414cd0d3207d504c6896fe1d0b549e9eebf
wack <kai.wang@citrix.com>
2017-05-08 14:38:35 +0800

    kmean cluster dest nodes.

--- initNetwork.py
+++ initNetwork.py
@@ -1,7 +1,12 @@
 # -*- coding:utf-8 -*-
 from node import Node
+from numpy import random
 
-
+'''
+生成网络
+arg: 结点数，x范围，y范围
+prt: 是否打印网络
+'''
 def init_network(number_of_node=100, width=1000, height=1000, prt=0):
 
     nodes = []
@@ -13,6 +18,19 @@ def init_network(number_of_node=100, width=1000, height=1000, prt=0):
         if prt:
             print "%d:(%d,%d) %f" % (i, nodes[i].x, nodes[i].y, nodes[i].energy)
 
-    print "_______init end__________"
+    # print "_______init end__________"
     return nodes
+
+# 生成目的节点
+def init_dest_node(nodes, number_of_dest, source_id, prt=0):
+    dest_id = []
+    number_of_node = len(nodes)
+    while len(dest_id) < number_of_dest:
+        x = random.randint(0, number_of_node)
+        if x != source_id and x not in dest_id:
+            dest_id.append(x)
+            if prt:
+                print "dest node is: %d (%d,%d) " % (x, nodes[x].x, nodes[x].y)
+    return dest_id
+
 
--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 # -*- coding:utf-8 -*-
 
-from node import Node
 from numpy import random
-from initNetwork import init_network
+from sklearn.cluster import KMeans
+from node import Node
+from initNetwork import init_network,init_dest_node
 from drawPic import *
 
 if __name__ == "__main__":
 
-    number_of_node = 100        # 传感网络节点数量
+    number_of_node = 100       # 传感网络节点数量
+    number_of_dest = 10
     width = 1000                # 长
     height = 1000               # 高
 
     source_id = random.randint(0, number_of_node)           # 源节点
     nodes = init_network(number_of_node, width, height)     # 生成网络
-    print "source node is: %d (%d,%d)" % (source_id, nodes[source_id].x, nodes[source_id].x)
-    dest_id = []
+    #print "source node is: %d (%d,%d)" % (source_id, nodes[source_id].x, nodes[source_id].x)
+    dest_id = init_dest_node(nodes,number_of_dest,source_id,0)
 
-    while len(dest_id) < number_of_node*0.1:
-        x = random.randint(0, number_of_node)  # 目的节点
-        if x != source_id and x not in dest_id:
-            dest_id.append(x)
-            print "dest node is: %d (%d,%d) " % (x, nodes[x].x, nodes[x].x)
-
-    draw_map(nodes, source_id, dest_id)
+    #draw_map(nodes, source_id, dest_id)
     #draw_energy(nodes)
 
     # 对目的节点聚类
+    feature = []
+    for id in dest_id:
+        temp = []
+        temp.append(nodes[id].x)
+        temp.append(nodes[id].y)
+        feature.append(temp)
+    #print feature
+
+    clf = KMeans(n_clusters=3)
+    s = clf.fit(feature)
+    cluster_center = clf.cluster_centers_
+    cluster = clf.labels_
 
 
 
