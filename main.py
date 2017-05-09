fe5b6f4b18eb271cb586bfc26e08b84e9fab1f76
imwack <i.am.wack.1993@gmail.com>
2017-05-09 20:45:15 +0800

    calculate next step

--- main.py
+++ main.py
@@ -30,53 +30,75 @@ def cluster(dest,clusters):
     return clf
 
 
-def calc_next_step(nodes, current_src, ):
-    # a = numpy.sqrt( pow(pos[0]-source.x,2) + pow(pos[1]-source.y,2) )
-    degree = []  # 角度
-    distance = []  # 距离
-    energy = []  # 能量
-    score = []  # 评分
-    candidate = []  # 候选节点
+def calc_next_step(nodes, source, pos):
+    temp_node = nodes[:]
+    print "dest:(%d,%d)" % (pos[0], pos[1])
     path = []  # 选择的路径
-    current_src = source
-    # source-dest:a  source-node:b  node-dest:c
-    for node in nodes:
-        if node.energy < 0.1:  # 能量过低舍去
-            continue
 
-        a = numpy.sqrt(pow(pos[0] - current_src.x, 2) + pow(pos[1] - current_src.y, 2))
-        b = numpy.sqrt(pow(node.x - current_src.x, 2) + pow(node.y - current_src.y, 2))
-        if b > numpy.sqrt(width * height) / 2:  # 距离过大直接舍去
-            continue
+    for time in range(1, 10):
+        # a = numpy.sqrt( pow(pos[0]-source.x,2) + pow(pos[1]-source.y,2) )
+        degree = []  # 角度
+        distance = []  # 距离
+        energy = []  # 能量
+        score = []  # 评分
+        candidate = []  # 候选节点
+        current_src = source
+        # source-dest:a  source-node:b  node-dest:c
+        for node in temp_node:
+            if node.id == current_src.id:
+                continue
 
-        c = numpy.sqrt(pow(pos[0] - node.x, 2) + pow(pos[1] - node.y, 2))
-        cos = (a ** 2 + c ** 2 - b ** 2) / 2 / a / c
-        d = math.acos((a * a + c * c - b * b) / 2 / a / c) * 180 / math.pi  # 角度degree = a2+c2-b2/2ac
-        if d > 90:  # 角度过大舍去
-            continue
+            if node.energy < 0.1:  # 能量过低舍去
+                continue
 
-        distance.append(b)
-        degree.append(d)
-        energy.append(node.energy)
-        candidate.append(node.id)
+            a = numpy.sqrt(pow(pos[0] - current_src.x, 2) + pow(pos[1] - current_src.y, 2))
+            b = numpy.sqrt(pow(node.x - current_src.x, 2) + pow(node.y - current_src.y, 2))
+            if b > numpy.sqrt(width * height) / 2:  # 距离过大直接舍去
+                #continue
+                pass
 
-    distance = normalization(distance)
-    degree = normalization(degree)
-    energy = normalization(energy)
-    score = (0.5 * distance + 0.5 * degree) / (energy + 0.0000001)  # 距离越小越好 角度越小越好 能量越大越好
-    # print score
-    next_step = 1  # 最终选择的下一跳
-    for i in range(len(score)):
-        if score[i] < score[next_step]:
-            next_step = i
-    path.append(candidate[next_step])
-    print "next step:%d (%d,%d)" % (candidate[next_step], nodes[candidate[next_step]].x, nodes[candidate[next_step]].y)
+            c = numpy.sqrt(pow(pos[0] - node.x, 2) + pow(pos[1] - node.y, 2))
+            cos = (a ** 2 + c ** 2 - b ** 2) / 2 / a / c
+            d = math.acos((a * a + c * c - b * b) / 2 / a / c) * 180 / math.pi  # 角度degree = a2+c2-b2/2ac
+            if d > 90:  # 角度过大舍去
+                continue
+
+            distance.append(b)
+            degree.append(d)
+            energy.append(node.energy)
+            candidate.append(node.id)
+
+
+        distance = normalization(distance)
+        degree = normalization(degree)
+        energy = normalization(energy)
+        score = (0.5 * distance + 0.5 * degree) / (energy + 0.0000001)  # 距离越小越好 角度越小越好 能量越大越好
+        #print candidate, score
+        next_step = 1  # 最终选择的下一跳 分数最小的
+        for i in range(len(score)):
+            if score[i] < score[next_step]:
+                next_step = i
+        path.append(candidate[next_step])
+        d = -1
+        for i in range(len(temp_node)):
+            if temp_node[i].id == candidate[next_step]:
+                d = i
+                break
+        if d!=-1:
+            del temp_node[d]
+
+        print "next step id:", candidate[next_step],"len node:",len(temp_node)
+        #print "next step:%d (%d,%d)" % (candidate[next_step], temp_node[candidate[next_step]].x, temp_node[candidate[next_step]].y)
+        for node in temp_node:
+            if node.id == candidate[next_step]:
+                source = node
+
 
 
 if __name__ == "__main__":
 
-    number_of_node = 1000        # 传感网络节点数量
-    number_of_dest = 100
+    number_of_node = 100        # 传感网络节点数量
+    number_of_dest = 10
     clusters = 3                # 类别个数
     width = 1000                # 长
     height = 1000               # 高
@@ -94,50 +116,9 @@ if __name__ == "__main__":
     clf = cluster(dest,clusters)
     cluster_center = clf.cluster_centers_
     dest_cluster = clf.labels_              # 目标节点分类
-    draw_after_cluster(nodes, source, dest, cluster_center, dest_cluster)
+    #draw_after_cluster(nodes, source, dest, cluster_center, dest_cluster)
 
     paths = []
     for pos in cluster_center:
-        # a = numpy.sqrt( pow(pos[0]-source.x,2) + pow(pos[1]-source.y,2) )
-        degree = []     # 角度
-        distance = []   # 距离
-        energy = []     # 能量
-        score = []      # 评分
-        candidate = []  # 候选节点
-        path = []       # 选择的路径
-        current_src = source
-        # source-dest:a  source-node:b  node-dest:c
-        for node in nodes:
-            if node.energy < 0.1:                # 能量过低舍去
-                continue
-
-            a = numpy.sqrt(pow(pos[0] - current_src.x, 2) + pow(pos[1] - current_src.y, 2))
-            b = numpy.sqrt(pow(node.x-current_src.x, 2) + pow(node.y-current_src.y, 2))
-            if b > numpy.sqrt(width*height)/2:    # 距离过大直接舍去
-                continue
-
-            c = numpy.sqrt(pow(pos[0]-node.x, 2) + pow(pos[1]-node.y, 2))
-            cos = (a**2+c**2-b**2)/2/a/c
-            d = math.acos((a*a+c*c-b*b)/2/a/c) * 180 / math.pi  # 角度degree = a2+c2-b2/2ac
-            if d > 90:                            # 角度过大舍去
-                continue
-
-            distance.append(b)
-            degree.append(d)
-            energy.append(node.energy)
-            candidate.append(node.id)
-
-        distance = normalization(distance)
-        degree = normalization(degree)
-        energy = normalization(energy)
-        score = (0.5*distance+0.5*degree)/(energy+0.0000001)         # 距离越小越好 角度越小越好 能量越大越好
-        #print score
-        next_step = 1  # 最终选择的下一跳
-        for i in range(len(score)):
-            if score[i] < score[next_step]:
-                next_step = i
-        path.append(candidate[next_step])
-        print "next step:%d (%d,%d)" % (candidate[next_step], nodes[candidate[next_step]].x, nodes[candidate[next_step]].y)
-        # print distance
-        # print degree
-        # print energy
+        calc_next_step(nodes, source, pos)
+    print len(nodes)
